0435fc7038d12993337d5e862d9a9bc39eaef333
Timo <timokubera@yahoo.com>
2021-01-13 02:45:38 +0100

    Updating instagram monitor

--- instagram/src/modify/instagram_monitor.py
+++ instagram/src/modify/instagram_monitor.py
@@ -1,6 +1,6 @@
 from instagram.data.config import *
 from instagram.src.helper import *
-from instagram.src.modify.modify_methods import pre_modify, compare_posts, compare_followers_following, compare_igtv, compare_hover_items
+from instagram.src.modify.modify_methods import pre_modify, compare_posts, compare_followers_following, compare_igtv,compare_tagged, compare_hover_items, compare_stories
 from instagram.src.instagram_object import InstagramObject
 
 class InstagramMonitor:
@@ -8,12 +8,13 @@ class InstagramMonitor:
         logger = logging.getLogger('instagram')
         logger.info("\n\n")
         logger.info("\t********MONITORING PHASE********")
-        #Wir untersuchen nur noch die subdirectories fuer jede einzelne Anfrage:
-        #Bspweise vergleichen wir nur die Posts Versionen miteinander, wenn auch der type Posts ist
-        #Sonst kommt es zu mehrfachen Vergleichen, weil fuer jeden einzelne url('type=posts', 'type=igtv', 'type=tagged')
-        #jedes mal alles durchgegangen wird(3*3=9 mal)
+        # We check only the subdirectories for every request. For example we check only the posts version
+        # with each other when also the type Posts is. Otherwise there would be more compares
+        # because for every url ('type=posts', 'type=igtv', 'type=tagged') every time everything would be compared.
+        # 3 * 3 = 9 times
+
         for url in monitoring_map["instagram"]:
-           
+
             html_type = get_type(url)
             old_html_path = get_old_html_path(url)
             new_html_path = get_new_html_path(url)
@@ -21,19 +22,25 @@ class InstagramMonitor:
             logger.info("compare ("+old_html_path+") with ("+new_html_path+")") 
             
             if not pre_modify(url):
-                logger.error("error while compare: "+old_html_path+" or "+new_html_path+" is missing")
+                logger.info("error while compare: "+old_html_path+" or "+new_html_path+" is missing")
+                set_change(url)
+                logger.debug(url)
                 continue
 
             ig = (InstagramObject(url, "new"), InstagramObject(url, "old"))
 
             compare_followers_following(url, ig)
+            compare_stories(url, ig)
+            # TODO throws lsit index out of bounds exception compare_stories(url, ig)
 
             if html_type == "posts":
                 compare_posts(url, ig)
             elif html_type == "igtv":
                 compare_igtv(url, ig)
-            
-            #compare_hover_items(url, ig)
+            elif html_type == "tagged":
+                compare_tagged(url, ig)
+            compare_hover_items(url, ig)
+            logger.debug(url)
 
 """ Circular import
 if __name__ == "__main__":
